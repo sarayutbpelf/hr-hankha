aaa2b3d4abd52feedff5027972bdae2d3b0d2088
sarayutbpelf <sarayutbpelf@gmail.com>
2026-08-04 19:40:44 +0700

    Delete js directory

--- js/pwa.js
+++
@@ -1,9 +0,0 @@
-/** ลงทะเบียน Service Worker เพื่อให้ใช้งานเป็น PWA ได้ (ติดตั้งลงหน้าจอ + ใช้งานออฟไลน์) */
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    const swUrl = new URL("service-worker.js", document.baseURI).href;
-    navigator.serviceWorker.register(swUrl).catch(() => {
-      /* เงียบไว้ถ้าลงทะเบียนไม่สำเร็จ (เช่น เปิดจาก file://) */
-    });
-  });
-}
